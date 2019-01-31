9ece1b1a72268ef0cc9d43b4d905543a93e8e198
Minwoo Park <minwoo33park@gmail.com>
2019-01-31 15:25:44 +0000

    make Node class as simple container

--- binarySearchTree/rope.py
+++ binarySearchTree/rope.py
@@ -15,62 +15,53 @@ class Node():
         else:
             self.length_sum = 0
 
-    def _get_weight(self):
-        """ get sum of the lengths of left subtree """
-        if self.left is None:
-            return 0
-        else:
-            return self.left.length_sum
-
-    def _index(self, idx):
-        """ returns the value at index idx from the subtree of self """
-        weight = self._get_weight()
-        if weight <= idx:
-            return self.right._index(idx - weight)
-        if self.left is not None:
-            return self.left._index(idx)
-        else:
-            return self.value[i]
-
-    @classmethod
-    def _concat_nodes(cls, left_node, right_node):
-        root = cls()
-        root.left = left_node
-        root.right = right_node
-        root.length_sum = left_node.length_sum + right_node.length_sum
-        return root
-
-    def _append(self, right_node):
-        return self._concat_nodes(self, right_node)
-    
-    def _append_left(self, left_node):
-        return self._concat_nodes(left_node, self)
-    
-    
-
 
 class Rope():
     def __init__(self):
         self.root = None
 
+    def _get_weight(self, node):
+        """ get sum of the lengths of left subtree """
+        if node.left is None:
+            return 0
+        else:
+            return node.left.length_sum
+
+    def _index(self, node, idx):
+        """ returns the value at index idx from the subtree of node """
+        weight = self._get_weight(node)
+        if weight <= idx:
+            return self._index(node.right, idx - weight)
+        if node.left is not None:
+            return self._index(node.left, idx)
+        else:
+            return node.value[i]
+
     def index(self, idx):
         """ returns the value at index idx """
-        return self.root._index(idx)
-    
+        return self._index(self.root, idx)
+
+    def _concat_nodes(self, left_node, right_node):
+        root = Node()
+        root.left = left_node
+        root.right = right_node
+        root.length_sum = left_node.length_sum + right_node.length_sum
+        return root
+
     @classmethod
-    def concat_ropes(cls, left_rope, right_rope):
+    def concat(cls, left_rope, right_rope):
         """ concatnate two ropes and return a new rope"""
         new_rope = cls()
-        new_rope.root = left_rope.root._append(right_rope.root)
+        new_rope.root = self._concat_nodes(left_rope.root, right_rope.root)
         return new_rope
     
     def append(self, right_rope):
-        """ append new rope in place """
-        self.root = self.root._append(right_rope.root)
-    
+        """ append a rope in place """
+        self.root = self._concat_nodes(self.root, right_rope.root)
+
     def append_left(self, left_rope):
-        """ preppend new rope in place """
-        self.root = left_rope.root._append(self.root)
+        """ preppend a rope in place """
+        self.root = self._concat_nodes(left_rope.root, self.root)
 
     
 n1 = Node()
