2652162f923f463c14351d8a9ab82e611bf0fe7f
Chanran Kim <bassilver@naver.com>
2019-02-11 23:28:31 +0900

    Update trie.py
    find and insert

--- Trie/trie.py
+++ Trie/trie.py
@@ -2,19 +2,19 @@ class Node():
   def __init__(self, key=None):
     self.key = key
     self.children = self
-    self.completeString = false;
+    self.complete_string = false;
 
 class Trie():
   def __init__(self, key_list):
     if len(key_list) < 1:
       return false;
-    self.rootNode = Node(key_list[0]);
+    self.root_node = Node(key_list[0]);
     for key in key_list[1:]:
       node = Node(key)
       self.insert(node)
       
-  def insert(key):
-    node = self.rootNode
+  def find(key):
+    node = self.root_node
     for char in key:
       if node.children[char]:
         node = node.childeren[char]
@@ -22,8 +22,16 @@ class Trie():
         return false;
     return true;
   
-  def find(key):
-    pass
+  def insert(key):
+    node = self.root_node
+    for char in key:
+      if node.children[char]:
+        node = node.childeren[char]
+      else:
+        new_node = Node(char)
+        node.childeren[char] = new_node
+        node = new_node
+    node.complete_string = true;
   
   def remove(key):
     pass
